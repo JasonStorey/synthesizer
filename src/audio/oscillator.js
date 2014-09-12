da1d8e33cbe16b0b5fd444c886457267a3a6c4e8
Jason Storey <jason.andrew.storey@gmail.com>
2014-09-12 23:27:59 +0100

    Remove digital clipping on Oscillator.stop()

--- src/audio/oscillator.js
+++ src/audio/oscillator.js
@@ -7,20 +7,24 @@ define([], function() {
     }
 
     Oscillator.prototype.start = function start() {
+        if(this._oscillator) {
+            this._oscillator.stop();
+        }
         this._oscillator = this.audioContext.createOscillator();
         this._oscillator.type = this.type;
         this._oscillator.frequency.value = this.frequency;
 
-        var gainNode = this.audioContext.createGain();
-        gainNode.gain.value = this.gain;
-        this._oscillator.connect(gainNode);
-        gainNode.connect(this.audioContext.getDestination());
+        this._gainNode = this.audioContext.createGain();
+        this._gainNode.gain.value = this.gain;
+        this._oscillator.connect(this._gainNode);
+        this._gainNode.connect(this.audioContext.getDestination());
 
         this._oscillator.start();
     };
 
     Oscillator.prototype.stop = function stop() {
-        this._oscillator.stop();
+        this._gainNode.gain.value = 0;
+        this._gainNode = null;
     };
 
     return Oscillator;
