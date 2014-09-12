08ac16b322df3c75319040beee18bb9c7fb0485f
Jason Storey <jason.andrew.storey@gmail.com>
2014-09-12 20:07:52 +0100

    Remove commented out code

--- src/audio/context.js
+++ src/audio/context.js
@@ -6,18 +6,7 @@ define(['utils/window'], function(window) {
         audioContext = new (window.AudioContext || window.webkitAudioContext)();
     }
 
-    function createOscillator(config) {
-//        var gainNode = audioContext.createGain(),
-//            oscillator = audioContext.createOscillator();
-//
-//        oscillator.type = config.type;
-//        oscillator.frequency.value = config.freq;
-//
-//        gainNode.gain.value = config.gain;
-//
-//        oscillator.connect(gainNode);
-//        gainNode.connect(audioContext.destination);
-//        return oscillator;
+    function createOscillator() {
         return audioContext.createOscillator();
     }
 
